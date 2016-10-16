8ae079863729e3d64a69b307af984c4da61ed312
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-10-16 16:39:57 -0700

    Finished routing the word add page and choose a story page, need to finish populating the words for rendering the madlib story page

--- madlibs.py
+++ madlibs.py
@@ -1,61 +1,104 @@
 from random import choice
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+
 
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
+app.secret_key = "rainy sunday"
+
 @app.route('/')
 def play_game():
     """Initiate game radio button"""
 
-    start_game = request.args.get("play")
-    
-    if start_game == "No":
-        return render_template("goodbye.html")
+    return render_template("index.html")
+
+@app.route("/choice",methods=["POST"])
+def choice():
+
+    shouldplay = request.form.get("play")
+
+    if shouldplay == "yes":
+        return redirect("/game")
     else:
-        return render_template("index.html")
+        return redirect("/goodbye")
+
+@app.route("/goodbye")
+def goodbye():
+    return render_template("goodbye.html")
+
+
+@app.route("/game")
+def game():
+
+    propernoun = session["proper_noun"]
+    noun = session["noun"]
+    verb = session["verb"]
+    adjective = session["adjective"]
+    adverb = session["adverb"]
+    exclamation = session["exclamation"]
+
+    return render_template("game.html", testdata=session, propernoun=propernoun, noun=noun, verb=verb, adjective=adjective, adverb=adverb, exclamation=exclamation)
+
+    #    start_game = request.args.get("play")
+    #
+
+    #if start_game == "No":
+    #return render_template("goodbye.html")
 
 @app.route('/game_add', methods=["POST"])
 def game_add():
     # I HAVE TO FIGURE OUT HOW TO GET NAME INTO THIS FUNCTION MANY MAKING NAME IN EACH BUTTON WHICH BUTTON IS BEING HIT
-    name = request.form.get("")
-    session[name] = session[name].get(name, [])
+    print request.form
+    word_type_list = request.form.get("name")
+    if word_type_list != "done":
 
-    propernoun_value = request.form.get("proper_noun")
-    session[name].append(propernoun_value)
+        word_type = request.form.get("name")
+        session[word_type] = session.get(word_type, [])
 
-    noun_value = request.form.get("noun")
-    session[name].append(noun_value)
+        if word_type == "proper_noun":
+            propernoun_value = request.form.get("proper_noun")
+            session[word_type].append(propernoun_value)
+        if word_type == "noun":
+            noun_value = request.form.get("noun")
+            session[word_type].append(noun_value)
+        if word_type =="verb":
+            verb_value = request.form.get("verb")
+            session[word_type].append(verb_value)
+        if word_type == "adjective":
+            adjective_value = request.form.get("adjective")
+            session[word_type].append(adjective_value)
+        if word_type == "adverb" :
+            adverb_value = request.form.get("adverb")
+            session[word_type].append(adverb_value)
+        if word_type == "exclamation" :
+            exclamation_value = request.form.get("exclamation")
+            session[word_type].append(exclamation_value)
+        return redirect("/game" )
+    else:
+        return redirect('/choose_story')
 
 
-    verb_value = request.form.get("verb")
-    session[name].append(verb_value)
+@app.route('/choose_story')
+def choose_story():
+    story_dictionary = { "/show_story/axe_murder" : "Axe Murder"}
+    
+    return render_template("choose_story.html", story_dictionary=story_dictionary)
 
-    ajective_value = request.form.get("adjective")
-    session[name].append(adjective_value)
+@app.route('/show_story/<story_name>')
+def show_story(story_name):
 
-    adverb_value = request.form.get("adverb")
-    session[name].append(adverb_verb)
+    if story_name == "axe_murder":
+       noun1 = choice(session["noun"])
+       
+
+       noun2 = choice(session["noun"])
+    return render_template(story_name + ".html")
 
 
-    return redirect("/game")
-
-
-@app.route('/madlib')
-def show_madlib():
-    """Generate madlib story"""
-
-    proper_noun = request.args.get("person")
-    color = request.args.get("favorite_color")
-    noun = request.args.get("noun")
-    adjective = request.args.get("adjective")
-    return render_template('madlib.html',person=proper_noun,
-                             favorite_color=color,
-                             noun=noun,
-                             adjective=adjective)
 
 
 
